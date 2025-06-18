eb8868e254d55ffd45032ea28e502e69e2bc9279
Hitesh Rathore <hitesh8003864620@gmail.com>
2025-06-18 05:41:38 +0530

    Add Assignment09 project

--- script.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById('uploadForm');
-const messageBox = document.getElementById('messageBox');
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent the form from submitting
-  messageBox.textContent = "Form submission is disabled for now.";
-});
